5cfd46fb9dd0bb98d4cfc392cce7ac36df3824ab
Rafael Rocha <rafael.rocha.mg@gmail.com>
2020-06-23 00:52:26 -0300

    Adding regex routing validation

--- public/index.php
+++ public/index.php
@@ -1,3 +1,21 @@
-<?php 
+<?php
+ini_set('display_errors', 1);
+require '../core/Router.php';
 
-echo "hello world!";
+$router = new Router();
+
+//Add the routes
+$router->add('', ['controller' => 'Home', 'action' => 'index']);
+$router->add('posts', ['controller' => 'Posts', 'action' => 'index']);
+$router->add('{controller}/{action}');
+$router->add('/admin/{controller}/{action}');
+$router->add('{controller}/{id:\d+}/{action}');
+
+$url = $_SERVER['QUERY_STRING'];
+
+if ($router->match($url)) {
+    echo '<pre> 123 ';
+    var_dump($router->getParams());
+} else {
+    echo 'No route found for URL ' . $url;
+}
